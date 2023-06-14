1faa5003caf79cc827e7999e19aa9c43971690f3
killshotxd <hassanansari211@gmail.com>
2023-06-14 17:09:55 +0530

    connecting redux to react with provider

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { users } from "./slices/UserSlice";
+import users from "./slices/UserSlice";
 
 const store = configureStore({
   reducer: {
